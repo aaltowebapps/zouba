199f49c77c365ce87d8c8d141cf1efde0d35ad99
Vilen Looga <vilen.looga@gmail.com>
2012-03-18 20:01:48 +0200

    Initial folder structure; RoR app initiated

---
+++ zouba-server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZoubaServer::Application.config.secret_token = '250bfb14ea0a065c56b2b828c43525ac031db31cca234953deb47d09dd453fdac9aa6caf2b28d6f0c636bbcb09e108a35677122089e090bcbcaecf577d35b882'
