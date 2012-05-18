65acbf730a584aa74b010ec83ecaa2656d9b2a89
evuski <evarionovoa@gmail.com>
2012-05-18 23:35:30 +0300

    Get current coordinates of user
    Is there a way to pre fill the start point placeholder with these
    coordinates? Will find out

---
+++ client/common/js/currentLocation.js
@@ -0,0 +1,15 @@
+var coords = {lat: "", lon: ""};
+
+function getLocation()
+  {
+  if (navigator.geolocation)
+    {
+    navigator.geolocation.getCurrentPosition(showPosition);
+    }
+  else{x.innerHTML="Geolocation is not supported by this browser.";}
+  }
+function showPosition(position)
+  {
+  coords.lat = position.coords.latitude;
+  coords.lon = position.coords.longitude;
+  }
